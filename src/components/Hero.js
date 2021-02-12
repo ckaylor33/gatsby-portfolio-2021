e2f36a6d61010d963921167e64fb41abf3ef616d
ckaylor33 <kaylor33@hotmail.co.uk>
2021-02-12 20:36:23 +0000

    initial commit - setup and basic styling of project finished - nav and hero also setup

---
+++ src/components/Hero.js
@@ -0,0 +1,44 @@
+import React from "react"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import SocialLinks from "../constants/socialLinks"
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childrenImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+const Hero = () => {
+  const {
+    file: {
+      childrenImageSharp: [{ fluid }],
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'm Charlie</h1>
+            <h4>freelance web developer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
+}
+
+export default Hero
